24976601074b383a9b9b85090e5ac4814a619112
Mayank Sureliya <mayank.sureliya@vidhaninfotech.com>
2025-05-12 19:40:58 +0530

    port changes

--- app.js
+++ app.js
@@ -18,5 +18,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(process.env.PORT || 3000, () => {
-	console.log(`Server running on port ${PORT}`);
+	console.log(`Server running on port ${process.env.PORT || 3000}`);
 });
